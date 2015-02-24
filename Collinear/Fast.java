5ece5c36ea084bd3af27bfa8cc3cea3855277659
Rui Dai <drstarry@gmail.com>
2015-02-24 01:10:15 -0800

    prepare for implementation

---
+++ Collinear/Fast.java
@@ -0,0 +1,12 @@
+/*
+A faster, sorting-based solution. Remarkably, it is possible to solve the problem much faster than the brute-force solution described above. Given a point p, the following method determines whether p participates in a set of 4 or more collinear points.
+
+Think of p as the origin.
+For each other point q, determine the slope it makes with p.
+Sort the points according to the slopes they makes with p.
+Check if any 3 (or more) adjacent points in the sorted order have equal slopes with respect to p. If so, these points, together with p, are collinear.
+Applying this method for each of the N points in turn yields an efficient algorithm to the problem. The algorithm solves the problem because points that have equal slopes with respect to p are collinear, and sorting brings such points together. The algorithm is fast because the bottleneck operation is sorting.
+*/
+public class Fast {
+   public static void main(String[] args)
+}
