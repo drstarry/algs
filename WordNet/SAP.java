24a83aa1dd4d73c743181ddf053b6e664e196313
Rui Dai <drstarry@gmail.com>
2015-04-12 01:26:49 -0700

    reformat code

--- WordNet/SAP.java
+++ WordNet/SAP.java
@@ -3,44 +3,102 @@
 
 // Performance requirements.  All methods (and the constructor) should take time at most proportional to E + V in the worst case, where E and V are the number of edges and vertices in the digraph, respectively. Your data type should use space proportional to E + V.
 
+import java.lang.IndexOutOfBoundsException;
+import java.lang.NullPointerException;
+
 public class SAP {
 
-   // constructor takes a digraph (not necessarily a DAG)
-   public SAP(Digraph G) {
+    private Digraph g;
+    private int root;
 
-   }
+    private class sapUnit {
 
-   // length of shortest ancestral path between v and w; -1 if no such path
-   public int length(int v, int w) {
-
-   }
-
-   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-   public int ancestor(int v, int w) {
-
-   }
-
-   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-   public int length(Iterable<Integer> v, Iterable<Integer> w) {
-
-   }
-
-   // a common ancestor that participates in shortest ancestral path; -1 if no such path
-   public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-
-   }
-
-   // do unit testing of this class
-   public static void main(String[] args) {
-    In in = new In(args[0]);
-    Digraph G = new Digraph(in);
-    SAP sap = new SAP(G);
-    while (!StdIn.isEmpty()) {
-        int v = StdIn.readInt();
-        int w = StdIn.readInt();
-        int length   = sap.length(v, w);
-        int ancestor = sap.ancestor(v, w);
-        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
     }
-}
-}
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        if (G == null) {
+            throw new NullPointerException();
+        }
+        g = G;
+        for (int i = 0; i < g.V(); i++) {
+            if (g.outdegree(i) == 0) {
+                root = i;
+                break;
+            }
+        }
+    }
+
+    private void swap(int v, int w) {
+        int temp = v;
+        v = w;
+        w = temp;
+    }
+
+    private String hashCode(int v, int w) {
+        if (v > w) {
+            swap(v, w);
+        }
+        return Integer.toString(v) + "&" + Integer.toString(w);
+    }
+
+    private boolean isValidArg(int a) {
+        if (a < 0 || a > g.V() - 1) {
+            return false;
+        }
+        else
+            return true;
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        if (!isValidArg(v) || !isValidArg(w)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        if (!isValidArg(v) || !isValidArg(w)) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null || w == null) {
+            throw new NullPointerException();
+        }
+        for (int vv: v) {
+            for (int ww: w) {
+
+            }
+        }
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null || w == null) {
+            throw new NullPointerException();
+        }
+
+        for (int vv: v) {
+            for (int ww: w) {
+
+            }
+        }
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length   = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
+    }
