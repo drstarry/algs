fbcc308957beceb5ced84a96131c8bd6cbdd633a
Rui Dai <drstarry@gmail.com>
2015-04-12 21:21:06 -0700

    complete outcast

--- WordNet/Outcast.java
+++ WordNet/Outcast.java
@@ -1,19 +1,41 @@
 // Measuring the semantic relatedness of two nouns. Semantic relatedness refers to the degree to which two concepts are related. Measuring semantic relatedness is a challenging problem. For example, most of us agree that George Bush and John Kennedy (two U.S. presidents) are more related than are George Bush and chimpanzee (two primates). However, not most of us agree that George Bush and Eric Arthur Blair are related concepts. But if one is aware that George Bush and Eric Arthur Blair (aka George Orwell) are both communicators, then it becomes clear that the two concepts might be related.
 
 public class Outcast {
+
+    private WordNet wordNet;
+
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
+        wordNet = wordnet;
     }
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
-
+        int maxDisc = Integer.MIN_VALUE;
+        String outcastWord = "";
+        for (String nounA: nouns) {
+            int disc = 0;
+            for (String nounB: nouns) {
+                if (nounA != nounB) {
+                    disc += wordNet.distance(nounA, nounB);
+                }
+            }
+            if (maxDisc < disc) {
+                maxDisc = disc;
+                outcastWord = nounA;
+            }
+        }
+        return outcastWord;
     }
 
     // see test client below
     public static void main(String[] args) {
-
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
     }
 }
-
