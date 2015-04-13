eab09ca4bdcadfebd826c061ba2c214c4f3174f5
Rui Dai <drstarry@gmail.com>
2015-04-12 21:22:54 -0700

    add nullpointer catcher

--- WordNet/Outcast.java
+++ WordNet/Outcast.java
@@ -1,5 +1,7 @@
 // Measuring the semantic relatedness of two nouns. Semantic relatedness refers to the degree to which two concepts are related. Measuring semantic relatedness is a challenging problem. For example, most of us agree that George Bush and John Kennedy (two U.S. presidents) are more related than are George Bush and chimpanzee (two primates). However, not most of us agree that George Bush and Eric Arthur Blair are related concepts. But if one is aware that George Bush and Eric Arthur Blair (aka George Orwell) are both communicators, then it becomes clear that the two concepts might be related.
 
+import java.lang.NullPointerException;
+
 public class Outcast {
 
     private WordNet wordNet;
@@ -11,6 +13,9 @@ public class Outcast {
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
+        if (nouns == null) {
+            throw new NullPointerException();
+        }
         int maxDisc = Integer.MIN_VALUE;
         String outcastWord = "";
         for (String nounA: nouns) {
