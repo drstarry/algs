3825c6233031266b84cd7b7df4bc8bf8ae42fa26
Rui Dai <drstarry@gmail.com>
2015-04-12 20:12:23 -0700

    modify sap

--- WordNet/SAP.java
+++ WordNet/SAP.java
@@ -5,15 +5,34 @@
 
 import java.lang.IndexOutOfBoundsException;
 import java.lang.NullPointerException;
+import java.util.Hashtable;
 
 public class SAP {
 
     private Digraph g;
     private int root;
+    private Hashtable<String, sapUnit> sapTable;
 
     private class sapUnit {
-
+        private int v; // vertice
+        private String id; // indentify whose ancestor
+        private int length; // the length of LCA path
+        public sapUnit(String id, int v, int length) {
+            if (id == null) {
+                throw new NullPointerException();
+            }
+            this.v = v;
+            this.id = id;
+            this.length = length;
+        }
+        public int v() {
+            return this.v;
+        }
+        public int len() {
+            return this.length;
+        }
     }
+
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
         if (G == null) {
@@ -26,6 +45,7 @@ public class SAP {
                 break;
             }
         }
+        sapTable = new Hashtable<String, sapUnit>();
     }
 
     private void swap(int v, int w) {
@@ -49,12 +69,76 @@ public class SAP {
             return true;
     }
 
+    // compute the LCA between v and w
+    private void computePairLCA(int v, int w) {
+        String id = hashCode(v, w);
+        if (!sapTable.containsKey(id)) {
+            BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(g, v);
+            BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(g, w);
+            int v2wLen = Integer.MAX_VALUE;
+            int w2vLen = Integer.MAX_VALUE;
+            int cur = w;
+            int ancestor = -1;
+            int length = Integer.MAX_VALUE;
+            int count = 0;
+            Hashtable<Integer, Integer> vVisited = new Hashtable<Integer, Integer>();
+            Hashtable<Integer, Integer> wVisited = new Hashtable<Integer, Integer>();
+
+            if (bfsV.hasPathTo(w)) {
+                v2wLen = bfsV.distTo(w);
+            }
+            if (bfsW.hasPathTo(v)) {
+                w2vLen = bfsW.distTo(v);
+            }
+
+            do {
+                if(bfsV.hasPathTo(cur)) {
+                    int curLen = bfsV.distTo(cur) + count;
+                    if (curLen < length) {
+                        length = curLen;
+                        ancestor = cur;
+                    } else if (curLen > length) {
+                        break;
+                    }
+                }
+                if (g.outdegree(cur) == 0)
+                    break;
+                count ++;
+                if (count > g.E())
+                    break;
+                cur = firstOfIterable(g.adj(cur));
+            } while (cur != w);
+
+            length = Math.min(length, Math.min(v2wLen, w2vLen));
+
+            if (length == Integer.MAX_VALUE) {
+                sapTable.put(id, new sapUnit(id, -1, -1));
+            }
+            else if (length == v2wLen) {
+                sapTable.put(id, new sapUnit(id, w, v2wLen));
+            }
+            else if (length == w2vLen) {
+                sapTable.put(id, new sapUnit(id, v, w2vLen));
+            }
+            else
+                sapTable.put(id, new sapUnit(id, ancestor, length));
+        }
+    }
+
+    private int firstOfIterable(Iterable<Integer> a) {
+        for (int v : a)
+            return v;
+        return Integer.MAX_VALUE;
+    }
+
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
         if (!isValidArg(v) || !isValidArg(w)) {
             throw new IndexOutOfBoundsException();
         }
-
+        String id = hashCode(v, w);
+        computePairLCA(v, w);
+        return sapTable.get(id).len();
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
@@ -62,6 +146,9 @@ public class SAP {
         if (!isValidArg(v) || !isValidArg(w)) {
             throw new IndexOutOfBoundsException();
         }
+        String id = hashCode(v, w);
+        computePairLCA(v, w);
+        return sapTable.get(id).v();
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
@@ -69,11 +156,19 @@ public class SAP {
         if (v == null || w == null) {
             throw new NullPointerException();
         }
+        int len = Integer.MAX_VALUE;
         for (int vv: v) {
             for (int ww: w) {
-
+                String id = hashCode(vv, ww);
+                computePairLCA(vv, ww);
+                if (sapTable.get(id).len() != -1)
+                    len = Math.min(len, sapTable.get(id).len());
             }
         }
+        if (len == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return len;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
@@ -81,12 +176,19 @@ public class SAP {
         if (v == null || w == null) {
             throw new NullPointerException();
         }
-
+        int len = Integer.MAX_VALUE;
+        int ver = -1;
         for (int vv: v) {
             for (int ww: w) {
-
+                String id = hashCode(vv, ww);
+                computePairLCA(vv, ww);
+                if (len > sapTable.get(id).len() && sapTable.get(id).len() != -1) {
+                    len = sapTable.get(id).len();
+                    ver = sapTable.get(id).v();
+                }
             }
         }
+        return ver;
     }
 
     // do unit testing of this class
@@ -102,3 +204,4 @@ public class SAP {
             StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
         }
     }
+}
