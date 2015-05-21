7a186a53a4bc47dc7766aafaa1eb281607c84c49
Rui Dai <drstarry@gmail.com>
2015-05-21 01:50:28 -0700

    struggle with timing... hehe

--- Boggle/BoggleSolver.java
+++ Boggle/BoggleSolver.java
@@ -13,11 +13,33 @@
 // 7               5
 // 8+              11
 
+// ------------------------------progress--------------------------------------
+// These are purely suggestions for how you might make progress. You do not have to follow these steps.
+
+// Familiarize yourself with the BoggleBoard.java data type.
+// Use a standard set data type to represent the dictionary, e.g., a SET<String>, a TreeSet<String>, or a HashSet<String>.
+// Create the data type BoggleSolver. Write a method based on depth-first search to enumerate all strings that can be composed by following sequences of adjacent dice. That is, enumerate all simple paths in the Boggle graph (but there is no need to explicitly form the graph). For now, ignore the special two-letter sequence Qu.
+// Now, implement the following critical backtracking optimization: when the current path corresponds to a string that is not a prefix of any word in the dictionary, there is no need to expand the path further. To do this, you will need to create a data structure for the dictionary that supports the prefix query operation: given a prefix, is there any word in the dictionary that starts with that prefix?
+// Deal with the special two-letter sequence Qu.
+
+// ------------------------------optimize--------------------------------------
+// You will likely need to optimize some aspects of your program to pass all of the performance points (which are, intentionally, more challenging on this assignment). Here are a few ideas:
+
+// Make sure that you have implemented the critical backtracking optimization described above. This is, by far, the most important step—several orders of magnitude!
+// Think about whether you can implement the dictionary in a more efficient manner. Recall that the alphabet consists of only the 26 letters A through Z.
+// Exploit that fact that when you perform a prefix query operation, it is usually alomst identical to the previous prefix query, except that it is one letter longer.
+// Precompute the Boggle graph, i.e., the set of cubes adjancent to each cube. But don't necessarily use a heavyweight Graph object.
+// Consider a nonrecursive implemention of the prefix query operation.
+// Consider a nonrecursive implemention of depth-first search.
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 
 public class BoggleSolver
 {
@@ -31,6 +53,9 @@ public class BoggleSolver
 
     private class BoggleDictionary {
         private DictionaryNode root;
+
+        public BoggleDictionary() {
+        }
 
         public void put(String s) {
             root = put(root, s, s, 0);
@@ -81,6 +106,32 @@ public class BoggleSolver
                 return get(x.mid, key, d + 1);
             else
                 return x;
+
+            // Stack<DictionaryNode> stack = new Stack<DictionaryNode>();
+            // stack.push(x);
+            // while (!stack.isEmpty()) {
+            //     char c = key.charAt(d);
+            //     x = stack.pop();
+            //     while (c < x.c) {
+            //         if (x.left == null)
+            //             return null;
+            //         stack.push(x.left);
+            //     }
+            //     while (c > x.c) {
+            //         if (x.right == null)
+            //             return null;
+            //         stack.push(x.right);
+            //     }
+            //     else if (d < key.length() - 1) {
+            //         if (x.mid == null)
+            //             return null;
+            //         stack.push(x.mid);
+            //         d++;
+            //     }
+            //     else
+            //         return x;
+            // }
+            // return x;
         }
     }
 
@@ -95,16 +146,20 @@ public class BoggleSolver
     private class BoggleWord {
         private HashSet<String> words;
         private Hashtable<String, Character> bd;
+        private Hashtable<String, Set<String>> surrounds;
         private int rows, cols;
 
         public BoggleWord(BoggleBoard board) {
             words = new HashSet<String>();
             bd = new Hashtable<String, Character>();
+            surrounds = new Hashtable<String, Set<String>>();
             rows = board.rows();
             cols = board.cols();
             for (int i = 0; i < rows; i++)
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < cols; j++) {
                     bd.put(getHash(i, j), board.getLetter(i, j));
+                    surrounds.put(getHash(i, j), getSurrounds(i, j));
+                }
         }
 
         private String getValue(char s) {
@@ -143,25 +198,27 @@ public class BoggleSolver
         }
 
         private void dfs(DictionaryNode curNode, String start, String cur, ArrayList<String> visited) {
+
             visited.add(cur);
             String str = curNode.val;
+
             if (str != null && str.length() > 2 && !words.contains(str)) {
                 words.add(str);
             }
 
-            for (String next: getSurrounds(cur)) {
+            for (String next: surrounds.get(cur)) {
                 if (!visited.contains(next)) {
                     String prefix = getValue(bd.get(next));
                     DictionaryNode nextNode = boggleTrie.get(curNode.mid, prefix, 0);
                     if (nextNode != null)
-                        dfs(nextNode, start, next, new ArrayList<String>(visited));
+                        dfs(nextNode, start, next, visited);
                 }
             }
+
+            visited.remove(cur);
         }
 
-        private Set<String> getSurrounds(String hashCode) {
-            int row = getX(hashCode);
-            int col = getY(hashCode);
+        private Set<String> getSurrounds(int row, int col) {
             Set<String> surrounds = new HashSet<String>();
             for (int i = row - 1; i <= row + 1; i++)
                 for (int j = col - 1; j <= col + 1; j++)
@@ -209,11 +266,14 @@ public class BoggleSolver
         BoggleSolver solver = new BoggleSolver(dictionary);
         BoggleBoard board = new BoggleBoard(args[1]);
         int score = 0;
+        long start = System.nanoTime();
         for (String word : solver.getAllValidWords(board))
         {
             StdOut.println(word);
             score += solver.scoreOf(word);
         }
+        long elapsedTime = System.nanoTime();
         StdOut.println("Score = " + score);
+        StdOut.println("Time = " + elapsedTime/1000000);
     }
 }
